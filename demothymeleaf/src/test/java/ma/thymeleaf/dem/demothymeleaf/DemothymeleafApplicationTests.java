dd7f35939ffb8e8bdc46549061fbcc9964e377d1
Ayajaid <106474047+Ayajaid@users.noreply.github.com>
2024-10-26 22:51:30 +0100

    Add files via upload

---
+++ demothymeleaf/src/test/java/ma/thymeleaf/dem/demothymeleaf/DemothymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package ma.thymeleaf.dem.demothymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemothymeleafApplicationTests {
+
+	
+	public static void main (String[] args) {
+		SpringApplication.run(DemothymeleafApplication.class, args);
+	}
+
+
+}
